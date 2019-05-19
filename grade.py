f49e56547c1ae91d982efdb98fbf21440ff26882
yyh-233 <807356837@qq.com>
2019-05-19 19:13:28 +0800

    yyh

---
+++ grade.py
@@ -0,0 +1,22 @@
+adict = {"小红":98,"小明":77,"小兰":86,"小花":30,"小惠":60,"二狗":60,"狗剩":77,"小华":80}
+s = 0
+bdict = { }
+cdict = { }
+klist = list(adict.keys())
+vlist = list(adict.values())
+for i in range(3):
+    cdict[klist[i]] = vlist[i]
+for i in range(3,len(adict)):
+    for j in cdict.keys():
+        if vlist[i] > cdict[j]:
+            cdict[klist[i]] = vlist[i]
+            del cdict[j]
+            break
+            
+for i in adict.keys():
+    s += adict[i]
+    if adict[i] < 60:
+        bdict[i] = adict[i]
+average = s/len(adict)
+print(cdict,average,bdict)
+    
---
+++ sort-maopao.py
@@ -0,0 +1,30 @@
+'''a = input()
+alist = [int(i) for i in a.split(' ')]'''
+alist = [1,4,2,0,8,4,2,3,10,1]
+
+'''alist = {'a':1,'b':4,'c':2,'d':0,'e':8,'f':4,'g':2,'h':3,'i':10,'j':1}'''
+if isinstance(alist,dict):
+    vlist = list(alist.values())
+    klist = list(alist.keys())
+    result = { }
+else:
+    vlist = alist
+
+for i in range(len(alist)-1):
+    for j in range(i+1,len(alist)):
+        if vlist[i] > vlist[j]:
+            b = vlist[i]            
+            vlist[i] = vlist[j]            
+            vlist[j] = b
+            if isinstance(alist,dict):
+                c = klist[i]
+                klist[i] = klist[j]
+                klist[j] = c
+if isinstance(alist,dict):
+    for i in range(len(alist)):
+        result[klist[i]] = vlist[i]
+    print(result)
+else:
+    print(vlist)
+
+
